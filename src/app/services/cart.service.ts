c7457a39401980cbf36b71f699d0ddfcbd079d27
Dmitry Veko <vekodmitriy0@gmail.com>
2020-09-29 13:34:43 +0300

    feat: add functionality with local storage for cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,13 +2,66 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from './product.service';
 import { OrderService } from './order.service';
+import { environment } from './../../environments/environment';
+import { CartModelPublic } from '../models/cart.model';
+import { CartModelServer } from './../models/cart.model';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { ProductModelServer } from './../models/product.model';
+import { throws } from 'assert';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  private serverURL = environment.SERVER_URL;
+  private cartDataClient: CartModelPublic = {
+    total: 0,
+    prodData: [{
+      incart: 0,
+      id: 0,
+    }],
+  };
+  private cartDataServer: CartModelServer = {
+    total: 0,
+    data: [{
+      numInCart: 0,
+      product: undefined,
+    }],
+  };
+  /*OBSERVABLE FOR COMPONENTS TO SUBSCRIBE */
+  cartTotal$ = new BehaviorSubject<number>(0);
+  cartData$ = new BehaviorSubject<CartModelServer>(this.cartDataServer);
+
   constructor(private http: HttpClient,
               private productService: ProductService,
-              private orderService: OrderService) { }
+              private orderService: OrderService,
+              private router: Router) {
+    this.cartTotal$.next(this.cartDataServer.total);
+    this.cartData$.next(this.cartDataServer);
+    const info: CartModelPublic = JSON.parse(localStorage.getItem('cart'));
+    if (info != null && info !== undefined && info.prodData[0].incart !== 0) {
+      this.cartDataClient = info;
+      this.cartDataClient.prodData.forEach(p => {
+        this.productService.getProductById(p.id).subscribe((actualProductInfo: ProductModelServer) => {
+          if (this.cartDataServer.data[0].numInCart === 0) {
+            this.cartDataServer.data[0].numInCart = p.incart;
+            this.cartDataServer.data[0].product = actualProductInfo;
+            this.cartDataClient.total = this.cartDataServer.total;
+            localStorage.setItem('cart', JSON.stringify(this.cartDataClient));
+          } else {
+            this.cartDataServer.data.push({
+              numInCart: p.incart,
+              product: actualProductInfo,
+            });
+            this.cartDataClient.total = this.cartDataServer.total;
+            localStorage.setItem('cart', JSON.stringify(this.cartDataClient));
+          }
+          this.cartData$.next({... this.cartDataServer});
+        });
+      });
+    }
+  }
+  
 }

47027ab4c61cd9859592cca995340a15f8352c09
Dmitry Veko <vekodmitriy0@gmail.com>
2020-10-01 21:47:09 +0300

    feat: add template for product component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ProductService } from './../../services/product.service';
+import { CartService } from 'src/app/services/cart.service';
+import { ActivatedRoute } from '@angular/router';
+
+declare let $: any;
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  id: number;
+  product;
+  thumbImages: any[] = [];
+
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private rout: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    	// Product Main img Slick
+	$('#product-main-img').slick({
+    infinite: true,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    fade: true,
+    asNavFor: '#product-imgs',
+  });
+
+	// Product imgs Slick
+  $('#product-imgs').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+		centerPadding: 0,
+		vertical: true,
+    asNavFor: '#product-main-img',
+		responsive: [{
+        breakpoint: 991,
+        settings: {
+					vertical: false,
+					arrows: false,
+					dots: true,
+        }
+      },
+    ]
+  });
+
+	// Product img zoom
+	var zoomMainProduct = document.getElementById('product-main-img');
+	if (zoomMainProduct) {
+		$('#product-main-img .product-preview').zoom();
+	}
   }
 
 }

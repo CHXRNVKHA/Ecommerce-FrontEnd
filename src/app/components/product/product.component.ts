a54074072da718217c637bb0f8a20498d8c16e5a
Dmitry Veko <vekodmitriy0@gmail.com>
2020-10-05 19:22:58 +0300

    final commit, for now...

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from './../../services/product.service';
 import { CartService } from 'src/app/services/cart.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 declare let $: any;
 
@@ -16,11 +17,29 @@ export class ProductComponent implements OnInit, AfterViewInit {
   product;
   thumbImages: any[] = [];
 
+  @ViewChild('quantity') quantityInput;
+
   constructor(private productService: ProductService,
               private cartService: CartService,
-              private rout: ActivatedRoute) { }
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        map((param: ParamMap) => {
+          // @ts-ignore
+          return param.params.id;
+        })
+      )
+      .subscribe(prodId => {
+        this.id = prodId;
+        this.productService.getProductById(this.id).subscribe(prod => {
+          this.product = prod;
+          if (prod.images !== null) {
+            this.thumbImages = prod.images.split(';');
+          }
+        });
+      });
   }
 
   ngAfterViewInit(): void {
@@ -60,6 +79,38 @@ export class ProductComponent implements OnInit, AfterViewInit {
 	if (zoomMainProduct) {
 		$('#product-main-img .product-preview').zoom();
 	}
+  }
+
+  addToCart(id: number): void {
+    this.cartService.addProductToCart(id, this.quantityInput.nativeElement.value);
+  }
+
+  increase(): void {
+    let value = parseInt(this.quantityInput.nativeElement.value, 10);
+
+    if (this.product.quantity >= 1) {
+      value++;
+      if (value > this.product.quantity) {
+        value = this.product.quantity;
+      }
+    } else {
+      return;
+    }
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  descrease(): void {
+    let value = parseInt(this.quantityInput.nativeElement.value, 10);
+
+    if (this.product.quantity > 0) {
+      value--;
+      if (value < 1) {
+        value = 1;
+      }
+    } else {
+      return;
+    }
+    this.quantityInput.nativeElement.value = value.toString();
   }
 
 }
